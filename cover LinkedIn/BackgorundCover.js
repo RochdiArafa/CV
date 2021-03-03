d080afeb6ea2a5dfaf3d04b690306dd83ab59596
Rachid Arafa <rachid.arafa21@gmail.com>
2021-03-03 11:33:49 +0100

    cover linkedIn

---
+++ cover LinkedIn/BackgorundCover.js
@@ -0,0 +1,17 @@
+
+
+
+var I        = {};
+I.am         = 'Web and mobile Engineer';
+I.love       = ['Programming','Problem solving','Fishing' ,'Anime','Challenge'];
+I.workedWith = ['Angular','React','Ionic','Symfony','Laravel','Spring Boot','Java EE','Node','Express','MongoDB','Mysql'];
+I.aspire     = 'to work with serious partners to accomplish ambitious projects';
+
+
+
+
+
+
+
+
+
